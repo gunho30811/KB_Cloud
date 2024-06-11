4a129687b3346c6731239677911f238ba4652ed7
LAGUNHO <gunho30811@naver.com>
2024-06-11 17:51:13 +0900

    return App.vue

--- FE/Cloud_duck/src/App.vue
+++ FE/Cloud_duck/src/App.vue
@@ -1,19 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <router-view></router-view>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-export default {
-  methods: {
-    initializeLayout() {
-      if (!this.$rtl.isRTL) {
-        // Just make sure rtl css is off when we are not on rtl
-        this.$rtl.disableRTL();
-      }
-    }
-  },
-  mounted() {
-    this.initializeLayout();
-  }
-};
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
