db0895f25e33e61975231459d9bf1b23be141ebc
dydrltk1379 <dydrltk1379@gachon.ac.kr>
2024-06-11 17:53:16 +0900

    commit

---
+++ FE/Cloud_duck/src/router/index.js
@@ -0,0 +1,7 @@
+import { createRouter, createWebHistory} from 'vue-router'
+import home from '@/pages/home.vue'; // 홈
+import expenses from '@/pages/expenses.vue'; // 상세 그래프
+import accountTable from '@/pages/accountTable.vue'; // 지출 상세 내역 테이블
+import calendar from '@/pages/calendar.vue'; // 캘린더
+import account from '@/pages/account.vue';  // portpolio
+import account from '@/pages/account.vue';
