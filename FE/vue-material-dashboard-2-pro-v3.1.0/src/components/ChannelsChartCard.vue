0b23eb369569e2a69b453a5eab86e43a15461785
dydrltk1379 <dydrltk1379@gachon.ac.kr>
2024-06-12 16:53:08 +0900

    commit

---
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/components/ChannelsChartCard.vue
@@ -0,0 +1,212 @@
+<template>
+  
+  <div class="card h-100">
+    <div class="col-md-6 col-12">
+        <div class="card z-index-2 mt-4">
+          <div class="card-header p-3 pt-2">
+            <div
+              class="icon icon-lg icon-shape bg-gradient-warning shadow-warning text-center border-radius-xl mt-n4 me-3 float-start"
+            >
+              <i class="material-icons opacity-10">donut_small</i>
+            </div>
+            <h6 class="mb-0">Doughnut chart</h6>
+            <p class="mb-0 text-sm">Affiliates program</p>
+          </div>
+          <div class="p-3 card-body">
+            <div class="card-body d-flex p-3 pt-0">
+              <doughnut-chart
+                class="w-50"
+                :chart="{
+                  labels: [
+                    'Dev.to',
+                    'Creative Tim',
+                    'Bootsnipp',
+                    'Github',
+                    'Codeinwp'
+                  ],
+                  datasets: {
+                    label: 'Projects',
+                    data: [15, 20, 12, 60, 20]
+                  }
+                }"
+              />
+              <!-- <div class="table-responsive w-50">
+                <table class="table align-items-center mb-0">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img
+                              src="../../assets/img/small-logos/devto.svg"
+                              class="avatar avatar-sm me-2"
+                              alt="logo_xd"
+                            />
+                          </div>
+                          <div
+                            class="d-flex flex-column justify-content-center"
+                          >
+                            <h6 class="mb-0 text-sm">DevTo</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="text-xs font-weight-bold">25%</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img
+                              src="../../assets/img/small-logos/creative-tim.svg"
+                              class="avatar avatar-sm me-2"
+                              alt="logo_atlassian"
+                            />
+                          </div>
+                          <div
+                            class="d-flex flex-column justify-content-center"
+                          >
+                            <h6 class="mb-0 text-sm">CreativeTim</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="text-xs font-weight-bold">13%</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img
+                              src="../../assets/img/small-logos/bootstrap.svg"
+                              class="avatar avatar-sm me-2"
+                              alt="logo_slack"
+                            />
+                          </div>
+                          <div
+                            class="d-flex flex-column justify-content-center"
+                          >
+                            <h6 class="mb-0 text-sm">Bootsnip</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="text-xs font-weight-bold">12%</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img
+                              src="../../assets/img/small-logos/github.svg"
+                              class="avatar avatar-sm me-2"
+                              alt="logo_spotify"
+                            />
+                          </div>
+                          <div
+                            class="d-flex flex-column justify-content-center"
+                          >
+                            <h6 class="mb-0 text-sm">Github</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="text-xs font-weight-bold">37%</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img
+                              src="../../assets/img/small-logos/google-webdev.svg"
+                              class="avatar avatar-sm me-2"
+                              alt="logo_jira"
+                            />
+                          </div>
+                          <div
+                            class="d-flex flex-column justify-content-center"
+                          >
+                            <h6 class="mb-0 text-sm">Codeinwp</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="text-xs font-weight-bold">13%</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="p-3 pb-0 card-header">
+      <div class="d-flex justify-content-between">
+        <h6 class="mb-0">분류별 지출</h6>
+      </div>
+    </div>
+    <div class="p-3 pb-0 mt-4 card-body">
+      <div class="row">
+        <div class="col-7 text-start">
+          <pie-chart
+            :chart="{
+              labels: ['Facebook', 'Direct', 'Organic', 'Referral'],
+              datasets: {
+                label: 'Projects',
+                data: [15, 20, 12, 60],
+              },
+            }"
+          />
+        </div>
+        <div class="my-auto col-5">
+          <span class="badge badge-md badge-dot me-4 d-block text-start">
+            <i class="bg-info"></i>
+            <span class="text-xs text-dark">Facebook</span>
+          </span>
+          <span class="badge badge-md badge-dot me-4 d-block text-start">
+            <i class="bg-primary"></i>
+            <span class="text-xs text-dark">Direct</span>
+          </span>
+          <span class="badge badge-md badge-dot me-4 d-block text-start">
+            <i class="bg-dark"></i>
+            <span class="text-xs text-dark">Organic</span>
+          </span>
+          <span class="badge badge-md badge-dot me-4 d-block text-start">
+            <i class="bg-secondary"></i>
+            <span class="text-xs text-dark">Referral</span>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="p-3 pt-0 pb-0 card-footer d-flex align-items-center">
+      <div class="w-60">
+        <p class="text-sm">
+          More than <b>1,200,000</b> sales are made using referral marketing,
+          and <b>700,000</b> are from social media.
+        </p>
+      </div>
+      <div class="w-40 text-end">
+        <a class="mb-0 btn bg-light text-end" href="javascript:;">Read more</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//C:\Front_End\KB_cloud\KB_Cloud\FE\vue-material-dashboard-2-pro-v3.1.0\src\views\dashboards\components\ChannelsChartCard.vue
+//C:\Front_End\KB_cloud\KB_Cloud\FE\vue-material-dashboard-2-pro-v3.1.0\src\views\pages\components\DoughnutChart.vue
+// import PieChart from "@/examples/Charts/PieChart.vue";
+// import DoughnutChart from "@/examples/Charts/DoughnutChart.vue";
+export default {
+  name: "ChannelsChartCard",
+  components: {
+    // PieChart,
+    // DoughnutChart
+  },
+};
+</script>
--- FE/vue-material-dashboard-2-pro-v3.1.0/src/pages/expenses/graph.vue
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/pages/expenses/graph.vue
@@ -1,28 +1,89 @@
 <template>
-    <div>
-      <Sidenav />
-      <div class="main-content">
-        <h1>graph</h1>
-        <!-- Account page content -->
+  <div class="py-4 container-fluid">
+    <div class="mt-4 row">
+      <div class="col-lg-4 col-sm-6">
+        <channels-chart-card />
+      </div>
+      <div class="mt-4 col-lg-8 col-sm-6 mt-sm-0">
+        <revenue-chart-card />
       </div>
     </div>
-  </template>
-  
-  <script>
-  import Sidenav from "@/components/Sidenav.vue";
-  
-  export default {
-    name: "Account",
-    components: {
-      Sidenav,
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .main-content {
-    margin-left: 250px; /* Adjust this value to match your sidebar width */
-    padding: 20px;
-  }
-  </style>
-  
+    <div class="mt-4 row">
+      <div class="col-12">
+        <orders-list-card
+          title="Top Selling Products"
+          :headers="['Product', 'Value', 'Ads Spent', 'Refunds']"
+          :lists="products"
+        />
+      </div>
+    </div>
+    <div class="mt-4 row">
+      <div class="col-12">
+        <orders-list-card
+          title="Top Selling Products"
+          :headers="['Product', 'Value', 'Ads Spent', 'Refunds']"
+          :lists="products"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChannelsChartCard from "@/components/ChannelsChartCard.vue";
+import RevenueChartCard from "@/components/RevenueChartCard.vue";
+import OrdersListCard from "@/examples/Cards/OrdersListCard.vue";
+
+// images
+import setTooltip from "@/assets/js/tooltip.js";
+
+export default {
+  name: "Sales",
+  components: {
+    ChannelsChartCard,
+    RevenueChartCard,
+    OrdersListCard,
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: "Nike v22 Running",
+          order: "12.821",
+          values: ["$130.992", "$9.500", "13"],
+          info: "Refund rate is lower with 97% than other products",
+          icon: "bold-down text-success",
+        },
+        {
+          title: "Business Kit (Mug + Notebook)",
+          order: "8.232",
+          values: ["$80.250", "$4.200", "40"],
+          icon: "bold-down text-success",
+        },
+        {
+          title: "Black Chair",
+          order: "2.421",
+          values: ["$40.600", "$9.430", "54"],
+          icon: "bold-up text-danger",
+        },
+        {
+          title: "Wireless Charger",
+          order: "5.921",
+          values: ["$91.300", "$7.364", "5"],
+          icon: "bold-down text-success",
+        },
+        {
+          title: "Mountain Trip Kit (Camera + Backpack)",
+          order: "921",
+          values: ["$140.925", "$20.531", "121"],
+          info: "Refund rate is higher with 70% than other products",
+          icon: "bold-up text-danger",
+        },
+      ],
+    };
+  },
+  mounted() {
+    setTooltip(this.$store.state.bootstrap);
+  },
+};
+</script>
