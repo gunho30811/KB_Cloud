bfc3a0b36123550ddd162d613358c707199eac10
hikhjin <hikhjin@gmail.com>
2024-06-12 12:42:09 +0900

    calendar

--- FE/vue-material-dashboard-2-pro-v3.1.0/src/views/applications/Calendar.vue
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/views/applications/Calendar.vue
@@ -86,7 +86,7 @@
       </div>
       <div class="col-xl-9">
         <div class="card card-calendar">
-          <fullCalendar ref="calendar" :options="calendarOptions" />
+          <calendar id="calendar-table" />
         </div>
       </div>
       <div class="col-xl-3">
@@ -218,133 +218,12 @@
 </template>
 
 <script>
-import swal from 'sweetalert2';
-import FullCalendar from "@fullcalendar/vue";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import Calendar from "../../examples/Calendar.vue";
 import EventsCard from "./components/EventsCard.vue";
 import MiniLineChart from "./components/MiniLineChart.vue";
-import { createEventId } from "./event-utils"; // Create this utility function if not already existing
 
 export default {
   name: "CalendarPage",
-  components: { FullCalendar, EventsCard, MiniLineChart },
-  data() {
-    return {
-      calendarOptions: {
-        plugins: [
-          dayGridPlugin,
-          timeGridPlugin,
-          interactionPlugin, // needed for dateClick
-        ],
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: ''
-        },
-        initialView: "dayGridMonth",
-        events: [
-          { id: createEventId(), title: '+23000\n-30000', start: '2021-01-01' },
-          { id: createEventId(), title: '+200\n-12000', start: '2021-01-02' },
-        ],
-        editable: true,
-        selectable: true,
-        select: this.handleDateSelect,
-        eventClick: this.handleEventClick,
-        eventContent: this.renderEventContent, // custom render function
-      },
-      currentEvents: [],
-    };
-  },
-  methods: {
-    handleDateSelect(selectInfo) {
-      const swalWithBootstrapButtons = swal.mixin({
-        customClass: {
-          confirmButton: "btn btn-success btn-fill",
-          cancelButton: "btn btn-danger btn-fill",
-        },
-        buttonsStyling: false,
-      });
-      swalWithBootstrapButtons
-        .fire({
-          title: "Create an Event",
-          html: `
-            <div>
-              <input type="text" id="income-input" placeholder="Income" class="swal2-input">
-              <input type="text" id="expense-input" placeholder="Expense" class="swal2-input">
-            </div>
-          `,
-          showCancelButton: true,
-        })
-        .then((result) => {
-          if (result.isConfirmed) {
-            const income = document.getElementById("income-input").value;
-            const expense = document.getElementById("expense-input").value;
-            let calendarApi = selectInfo.view.calendar;
-            calendarApi.unselect(); // clear date selection
-
-            if (income || expense) {
-              const title = `${income ? '+' + income : ''}\n${expense ? '-' + expense : ''}`;
-              calendarApi.addEvent({
-                id: createEventId(),
-                title,
-                start: selectInfo.startStr,
-                end: selectInfo.endStr,
-                allDay: selectInfo.allDay,
-              });
-            }
-          }
-        });
-    },
-    handleEventClick(clickInfo) {
-      if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-        clickInfo.event.remove();
-      }
-    },
-    renderEventContent(eventInfo) {
-      const lines = eventInfo.event.title.split('\n');
-      return {
-        html: lines.map(line => `<div class="custom-event-content" style="color: ${line.startsWith('+') ? 'blue' : 'red'};">${line}</div>`).join('')
-      }
-    },
-    handleEvents(events) {
-      this.currentEvents = events;
-    },
-  },
+  components: { Calendar, EventsCard, MiniLineChart },
 };
 </script>
-
-<style>
-#fullCalendar {
-  min-height: 400px;
-}
-
-.el-loading-spinner .path {
-  stroke: #66615b !important;
-}
-
-.custom-event-content {
-  background-color: white;
-}
-
-/* Custom styles for FullCalendar buttons */
-.fc-button {
-  background-color: blue;
-  border: none;
-  color: white;
-}
-
-.fc-button:hover {
-  background-color: darkblue;
-}
-
-.fc-button-primary:not(:disabled).fc-button-active, .fc-button-primary:not(:disabled):active {
-  background-color: darkblue;
-}
-
-.fc-button-primary {
-  background-color: blue;
-  border-color: blue;
-}
-</style>
