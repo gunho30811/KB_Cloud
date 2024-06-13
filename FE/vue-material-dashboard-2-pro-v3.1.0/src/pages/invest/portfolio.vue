7f1a2b67c86cdc07888a91f6b464f68fd91860e7
LAGUNHO <gunho30811@naver.com>
2024-06-13 15:44:04 +0900

    portfoli pages sidenav 추가

--- FE/vue-material-dashboard-2-pro-v3.1.0/src/pages/invest/portfolio.vue
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/pages/invest/portfolio.vue
@@ -1,15 +1,24 @@
 <template>
-    <RateWorld/>
+  <Sidenav />
+  <div class="rate">
+    <RateWorld />
+  </div>
 </template>
 
 <script>
 import RateWorld from "@/components/rate_world.vue";
+import Sidenav from "@/components/Sidenav.vue";
 
 export default {
   name: 'portfolio,vue',
   components: {
-    RateWorld
+    RateWorld, Sidenav
   }
 };
 </script>
 
+<style scoped>
+.rate{
+  margin-left:120px ;
+}
+</style>
