6a129e4cb5ee276c69b27fa425e2ef64156f603d
hikhjin <hikhjin@gmail.com>
2024-06-13 21:49:05 +0900

    assetGraph.vue 추가

---
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/components/AssetGraph.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="card z-index-2 mt-4">
+      <div class="card-header p-3 pt-2">
+        <div class="text-center border-radius-xl mt-n4 me-3 float-start"></div>
+        <div class="d-block d-md-flex">
+          <div class="me-auto">
+            <h6 class="mb-0">오늘까지 40만원 벌었어요</h6>
+          </div>
+          <span class="badge badge-lg badge-dot me-4 d-inline-block text-start">
+            <i class="bg-success"></i>
+            <span class="text-dark">남은 자산</span>
+          </span>
+          <span class="badge badge-lg badge-dot me-4 d-inline-block text-start">
+            <i class="bg-dark"></i>
+            <span class="text-dark">투자 이익</span>
+          </span>
+        </div>
+      </div>
+      <div class="p-3 card-body">
+        <div class="chart">
+          <default-line-chart
+            id="line-chart"
+            title="Line chart"
+            :chart="{
+              labels: [
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+              ],
+              datasets: [
+                {
+                  label: '남은 자산',
+                  data: [50, 40, 30, 22, 50, 20, 40, 23, 50]
+                },
+                {
+                  label: '투자 이익',
+                  data: [30, 90, 40, 140, 290, 290, 340, 230, 400]
+                }
+              ]
+            }"
+          />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    import DefaultLineChart from "@/components/DefaultLineChart.vue";
+  
+  export default {
+    name: "AssetGraph",
+    components: {
+      DefaultLineChart,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- FE/vue-material-dashboard-2-pro-v3.1.0/src/pages/expenses/main.vue
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/pages/expenses/main.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="container">
     <Sidenav />
 
     <!--자산 내역 그래프-->
-    <div class="py-4 container col-9">
+    <div class="main-content py-4 container-fluid">
       <h1>Main</h1>
       <div class="row">
         <div class="col-md-6">
@@ -15,55 +15,7 @@
       </div>
       <div class="mt-1 row">
         <div class="col-12">
-          <div class="card z-index-2 mt-4">
-            <div class="card-header p-3 pt-2">
-              <div class="text-center border-radius-xl mt-n4 me-3 float-start"></div>
-              <div class="d-block d-md-flex">
-                <div class="me-auto">
-                  <h6 class="mb-0">오늘까지 40만원 벌었어요</h6>
-                </div>
-                <span class="badge badge-lg badge-dot me-4 d-inline-block text-start">
-                  <i class="bg-success"></i>
-                  <span class="text-dark">남은 자산</span>
-                </span>
-                <span class="badge badge-lg badge-dot me-4 d-inline-block text-start">
-                  <i class="bg-dark"></i>
-                  <span class="text-dark">투자 이익</span>
-                </span>
-              </div>
-            </div>
-            <div class="p-3 card-body">
-              <div class="chart">
-                <default-line-chart
-                  id="line-chart"
-                  title="Line chart"
-                  :chart="{
-                    labels: [
-                      'Apr',
-                      'May',
-                      'Jun',
-                      'Jul',
-                      'Aug',
-                      'Sep',
-                      'Oct',
-                      'Nov',
-                      'Dec'
-                    ],
-                    datasets: [
-                      {
-                        label: '남은 자산',
-                        data: [50, 40, 30, 22, 50, 20, 40, 23, 50]
-                      },
-                      {
-                        label: '투자 이익',
-                        data: [30, 90, 40, 140, 290, 290, 340, 230, 400]
-                      }
-                    ]
-                  }"
-                />
-              </div>
-            </div>
-          </div>
+          <AssetGraph />
         </div>
 
       <!--수입/지출 상세 내역-->
@@ -263,12 +215,14 @@
 <script>
 import Sidenav from "@/components/Sidenav.vue";
 import DefaultLineChart from "@/components/DefaultLineChart.vue";
+import AssetGraph from "@/components/AssetGraph.vue";
 
 export default {
   name: "MainPage",
   components: {
     Sidenav,
     DefaultLineChart,
+    AssetGraph,
   },
 };
 </script>
