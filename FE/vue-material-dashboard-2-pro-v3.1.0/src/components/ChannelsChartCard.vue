f8ca806df544c9b7f32df918d58613408d8a8aaa
dydrltk1379 <dydrltk1379@gachon.ac.kr>
2024-06-14 12:42:35 +0900

    commit

--- FE/vue-material-dashboard-2-pro-v3.1.0/src/components/ChannelsChartCard.vue
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/components/ChannelsChartCard.vue
@@ -1,101 +1,40 @@
-<template>  
-  <div class=" container-fluid">
+<template>
+  <div class="container-fluid">
     <div class="row">
       <div>
         <div class="card z-index-2">
-          <div class="card-header p-3 pt-2">       
+          <div class="card-header p-3 pt-2">
             <h6 class="mb-0">분류별 지출</h6>
             <p class="mb-0 text-sm">분류별 지출 내역을 도넛 차트로 살펴보세요.</p>
           </div>
           <div class="p-3 card-body">
-            <div class="card-body d-flex p-3 pt-0">
-              <doughnut-chart
-                class="w-50"
-                :chart="{
-                  labels: [
-                    '식비',
-                    '의료',
-                    '교통비',
-                    '생활용품',
-                    '기타'
-                  ],
-                  datasets: {
-                    label: 'Projects',
-                    data: [25, 13, 12, 37, 13]
-                  }
-                }"
-              />
-              <div class="table-responsive w-50">
+            <div class="card-body d-flex p-1 pt-0">
+              <doughnut-chart class="w-50" :chart="chartData" />
+              <div class="table-responsive w-20">
                 <table class="table align-items-center mb-0">
                   <tbody>
-                    <tr>
+                    <tr v-for="(item, index) in chartData.labelsToShow" :key="index">
                       <td>
                         <div class="d-flex px-2 py-1">
-                          <div
-                            class="d-flex flex-column justify-content-center"
-                          >
-                            <h6 class="mb-0 text-sm">식비</h6>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">{{ item.label }}</h6>
                           </div>
                         </div>
                       </td>
-                      <td class="">
-                        <span class="text-xs font-weight-bold">25%</span>
+                      <td class="align-middle text-center text-sm text-right">
+                        <span class="text-xs font-weight-bold">{{ item.data }}%</span>
                       </td>
                     </tr>
-                    <tr>
+                    <tr v-if="chartData.otherCategory">
                       <td>
                         <div class="d-flex px-2 py-1">
-                          <div
-                            class="d-flex flex-column justify-content-center"
-                          >
-                            <h6 class="mb-0 text-sm">의료</h6>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="align-middle text-center text-sm">
-                        <span class="text-xs font-weight-bold">13%</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-flex px-2 py-1">
-                          <div
-                            class="d-flex flex-column justify-content-center"
-                          >
-                            <h6 class="mb-0 text-sm">교통비</h6>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="align-middle text-center text-sm">
-                        <span class="text-xs font-weight-bold">12%</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-flex px-2 py-1">
-                          <div
-                            class="d-flex flex-column justify-content-center"
-                          >
-                            <h6 class="mb-0 text-sm">생활용품</h6>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="align-middle text-center text-sm">
-                        <span class="text-xs font-weight-bold">37%</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-flex px-2 py-1">
-                          <div
-                            class="d-flex flex-column justify-content-center"
-                          >
+                          <div class="d-flex flex-column justify-content-center">
                             <h6 class="mb-0 text-sm">기타</h6>
                           </div>
                         </div>
                       </td>
-                      <td class="align-middle text-center text-sm">
-                        <span class="text-xs font-weight-bold">13%</span>
+                      <td class="align-middle text-center text-sm text-right">
+                        <span class="text-xs font-weight-bold">{{ chartData.otherCategory }}%</span>
                       </td>
                     </tr>
                   </tbody>
@@ -111,5 +50,57 @@
 
 <script setup>
 import DoughnutChart from "@/examples/Charts/DoughnutChart.vue";
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
+const chartData = ref({
+  labelsToShow: [],
+  otherCategory: 0,
+  datasets: [
+    {
+      label: 'Projects',
+      data: [],
+      backgroundColor: ['#03A9F4', '#3A416F', '#fb8c00', '#a8b8d8', '#e91e63'],
+    }
+  ],
+});
+
+onMounted(async () => {
+  await fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('/api/expenses');
+    const members = response.data;
+
+    const categoryMap = new Map();
+    members.forEach(member => {
+      if (categoryMap.has(member.category)) {
+        categoryMap.set(member.category, categoryMap.get(member.category) + member.money);
+      } else {
+        categoryMap.set(member.category, member.money);
+      }
+    });
+
+    const sortedCategories = Array.from(categoryMap.entries())
+      .map(([category, totalMoney]) => ({ label: category, data: totalMoney }))
+      .sort((a, b) => b.data - a.data);
+
+    const totalMoney = sortedCategories.reduce((acc, category) => acc + category.data, 0);
+    chartData.value.labelsToShow = sortedCategories.slice(0, 4).map(category => ({
+      label: category.label,
+      data: ((category.data / totalMoney) * 100).toFixed(2),
+    }));
+
+    const otherCategories = sortedCategories.slice(4);
+    if (otherCategories.length > 0) {
+      chartData.value.otherCategory = otherCategories.reduce((acc, category) => acc + ((category.data / totalMoney) * 100), 0).toFixed(2);
+    }
+
+    chartData.value.datasets[0].data = chartData.value.labelsToShow.map(item => item.data);
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+  }
+};
 </script>
