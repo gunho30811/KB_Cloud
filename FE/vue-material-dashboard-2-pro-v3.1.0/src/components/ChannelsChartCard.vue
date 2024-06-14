6fa43147f1ec6f4a407f45a6032c9321cabe1dc1
dydrltk1379 <dydrltk1379@gachon.ac.kr>
2024-06-14 16:15:55 +0900

    chartCard update

--- FE/vue-material-dashboard-2-pro-v3.1.0/src/components/ChannelsChartCard.vue
+++ FE/vue-material-dashboard-2-pro-v3.1.0/src/components/ChannelsChartCard.vue
@@ -53,28 +53,13 @@ import DoughnutChart from "@/examples/Charts/DoughnutChart.vue";
 import { ref } from 'vue';
 import axios from 'axios';
 
-// const chartData = ref({
-//   labelsToShow: [],
-//   otherCategory: 0,
-//   datasets: [
-//     {
-//       label: 'Projects',
-//       data: [1,2,3],
-//     }
-//   ],
-// });
-
 const chartData = ref({
-                  labels: [],
-                  datasets: {
-                    label: 'Projects',
-                    data: []
-                  }
-                });
-
-// onMounted(async () => {
-//   await fetchData();
-// });
+  labels: [],
+  datasets: {
+    label: 'Projects',
+    data: []
+  }
+});
 
 const fetchData = async () => {
   try {
@@ -91,56 +76,32 @@ const fetchData = async () => {
           }
         });
 
-    // 정렬
     const sortedCategories = Array.from(categoryMap.entries())
       .map(([category, totalMoney]) => ({ label: category, data: totalMoney }))
       .sort((a, b) => b.data - a.data);
 
     const totalMoney = sortedCategories.reduce((acc, category) => acc + category.data, 0);
 
-    // 4개까지 자르기
     const labelsToShow = sortedCategories.slice(0, 4).map(category => ({
       label: category.label,
       data: ((category.data / totalMoney) * 100).toFixed(2),
     }));
-    // chartData.value = sortedCategories.slice(0, 4).map(category => ({
-    //   label: category.label,
-    //   data: ((category.data / totalMoney) * 100).toFixed(2),
-    // }));
-    // chartData.value.labelsToShow = sortedCategories.slice(0, 4).map(category => ({
-    //   label: category.label,
-    //   data: ((category.data / totalMoney) * 100).toFixed(2),
-    // }));
 
     const otherCategories = sortedCategories.slice(4);
-    // chartData.value.otherCategory = otherCategories.reduce((acc, category) => acc + ((category.data / totalMoney) * 100), 0).toFixed(2);
-    // otherCategory.value = otherCategories.reduce((acc, category) => acc + ((category.data / totalMoney) * 100), 0).toFixed(2);
     const otherCategory = otherCategories.reduce((acc, category) => acc + ((category.data / totalMoney) * 100), 0).toFixed(2);
+    labelsToShow.push({
+      label: '기타',
+      data: otherCategory,
+    });
 
-    // chartData.value.datasets[0].data = chartData.value.labelsToShow.map(item => item.data);
-    chartData.value = {
-      // labels: sortedCategories.slice(0, 4).map(category => category.label),
-      labelsToShow,
-      otherCategory,
-      datasets: [
-        // { data: labelsToShow.map(item => item.data)}
-        {data: labelsToShow.map(item => parseFloat(item.data))}
-      ]
+
+    chartData.value = { 
+      labels:labelsToShow.map(item => item.label),
+      datasets:{
+        data:labelsToShow.map(item => parseFloat(item.data))
+      },
+      labelsToShow
     };
-
-  chartData.value = {
-                  labels: [
-                    'Dev.to',
-                    'Creative Tim',
-                    'Bootsnipp',
-                    'Github',
-                    'Codeinwp'
-                  ],
-                  datasets: {
-                    label: 'Projects',
-                    data: [50, 40, 10, 0, 0]
-                  }
-                };
 
   console.log('chartData:', chartData.value);
   } catch (error) {
